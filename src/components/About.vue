f766b5e95ba99bc23f4aa8a0113394696fd4049c
miguelmapa7 <miguel.perezar@cecar.edu.co>
2021-11-10 12:46:25 -0500

    algunos arreglos

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,15 @@
 <template>
-<div class="row" id="cards" >
-    <div class="col-4" 
-    v-for="item in items" 
-    :key="item.name" >
-  
+  <div class="row" id="cards">
+    <div class="col-4" v-for="item in items" :key="item.name">
       <v-card class="mx-auto" max-width="344" id="card">
-        <v-img
-          :src= "item.image"
-          height="300"
-          class="grey darken-4"
-        ></v-img>
+        <v-img :src="item.image" max-height="340" class="grey darken-4"></v-img>
 
-        <v-card-title> {{item.name}}</v-card-title> 
+        <v-card-title> {{ item.name }}</v-card-title>
 
-        <v-card-subtitle> {{item.profetion}} </v-card-subtitle>
+        <v-card-subtitle> {{ item.profetion }} </v-card-subtitle>
 
         <v-card-actions>
-          <v-btn color="orange lighten-2" text> About Him </v-btn>
+          <v-btn color="orange lighten-2" text> Información </v-btn>
 
           <v-spacer></v-spacer>
 
@@ -26,16 +19,25 @@
         </v-card-actions>
 
         <v-expand-transition>
-          <div v-show="show">
+          <div v-show="show" style="margin-left: 10px;">
             <v-divider></v-divider>
+            <v-card-text> {{ item.infomation }} </v-card-text>
+            <div class="row" style=" margin: 2px;">
+              
+              <div class="" style=" padding: 4%; ">
+                <v-icon>{{ item.icon }}</v-icon>
+              </div>
+              <div class="" >
+                <v-card-text> {{ item.email }} </v-card-text>
+              </div>
+            </div>
 
-            <v-card-text> {{item.infomation}} </v-card-text>
+            <v-divider></v-divider>
           </div>
         </v-expand-transition>
       </v-card>
-    
+    </div>
   </div>
-   </div>
 </template>
 
 <script>
@@ -44,16 +46,21 @@ export default {
     show: false,
     items: [
       {
+        name: "Carlos Cohen",
+        image: require("@/assets/carlos-cohen.png"),
+        profetion: "Software developer engineer and teacher Cecar",
+        infomation:
+          "MAGÍSTER INGENIERÍA DE SISTEMAS Y COMPUTACIÓN, PHD (C) PROYECTOS DE INGENIERÍA BASADOS EN TICS",
+        icon: "mdi-email",
+        email: "carlos.cohen@cecar.edu.co",
+      },
+      {
         name: "Miguel Andrés Pérez Arcia",
         image: require("@/assets/FotoPerfil.jpeg"),
         profetion: "Software developer engineer",
         infomation: "Software developer",
-      },
-      {
-        name: "Carlos Cohen",
-        image: require("@/assets/ahorrar-agua.png"),
-        profetion: "Software developer engineer and teacher Cecar",
-        infomation: "Teacher Cecar",
+        icon: "mdi-email",
+        email: "miguel.perezar@cecar.edu.co",
       },
     ],
   }),
